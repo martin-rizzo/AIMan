651a78ac09ad70d7872e5745e51a6339e7ffe8ec
Martin Rizzo <martinrizzo@gmail.com>
2023-11-28 22:33:17 -0300

    feat: Implement installation and execution for "comfyui"
    This commit enable the installation and launch functionality for
    the "comfyui" project. Users can now conveniently manage the
    project using the commands:
    > aiman install comfyui
    > aiman launch comfyui

---
+++ System/code/project-comfyui.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env bash
+# File    : project-comfyui.sh
+# Brief   : Controls the local copy of the "comfyui" project.
+# Author  : Martin Rizzo | <martinrizzo@gmail.com>
+# Date    : Nov 28, 2023
+# Repo    : https://github.com/martin-rizzo/AIMan
+# License : MIT
+#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+#                                    AIMan
+#        A basic package management system for AI open source projects
+#   
+#     Copyright (c) 2023 Martin Rizzo
+#     
+#     Permission is hereby granted, free of charge, to any person obtaining
+#     a copy of this software and associated documentation files (the
+#     "Software"), to deal in the Software without restriction, including
+#     without limitation the rights to use, copy, modify, merge, publish,
+#     distribute, sublicense, and/or sell copies of the Software, and to
+#     permit persons to whom the Software is furnished to do so, subject to
+#     the following conditions:
+#     
+#     The above copyright notice and this permission notice shall be
+#     included in all copies or substantial portions of the Software.
+#     
+#     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+#     EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+#     MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+#     IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+#     CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+#     TORT OR OTHERWISE, ARISING FROM,OUT OF OR IN CONNECTION WITH THE
+#     SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
+
+
+
+function install() {
+    local project_dir=$(print_project @directory)
+
+    require_system_command git
+    require_virtual_python
+    
+    change_to_repo_directory
+    clone_project_to "$project_dir"
+    cd "$project_dir"
+    require_soft_link 'models/checkpoints' '../../Output' 1
+    
+    # export INVOKEAI_ROOT=$project_dir
+
+    # NVIDIA GPU
+    virtual_python !pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu121
+    
+    # Dependencies
+    virtual_python !pip install -r requirements.txt
+    
+}
+
+function launch() {
+    local project_dir=$(print_project @directory)
+    local options=("$@")
+
+    change_to_repo_directory
+    cd "$project_dir"
+    virtual_python main.py "${options[@]}"
+}
