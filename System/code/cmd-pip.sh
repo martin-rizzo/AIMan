1dd9fecd05d277bcffb019f304fedf9635242c80
Martin Rizzo <martinrizzo@gmail.com>
2024-01-23 13:41:22 -0300

    feat: Introduce 'aiman pip' command for the python venv management
    This commit enhances the 'aiman' script by introducing the 'aiman pip' command.
    This feature allows users to execute pip within the Python virtual environment,
    simplifying the installation and administration of packages for the third-party
    open-source AI projects.

---
+++ System/code/cmd-pip.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+# File    : cmd-pip.sh
+# Brief   : Command to install, manage, and remove Python packages using 'pip'
+# Author  : Martin Rizzo | <martinrizzo@gmail.com>
+# Date    : Jul 24, 2023
+# Repo    : https://github.com/martin-rizzo/AIMan
+# License : MIT
+#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+#                                    AIMan
+#        A basic package management system for AI open source projects
+#   
+#     Copyright (c) 2023-2024 Martin Rizzo
+#     
+#     Permission is hereby granted, free of charge, to any person obtaining
+#     a copy of this software and associated documentation files (the
+#     "Software"), to deal in the Software without restriction, including
+#     without limitation the rights to use, copy, modify, merge, publish,
+#     distribute, sublicense, and/or sell copies of the Software, and to
+#     permit persons to whom the Software is furnished to do so, subject to
+#     the following conditions:
+#     
+#     The above copyright notice and this permission notice shall be
+#     included in all copies or substantial portions of the Software.
+#     
+#     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+#     EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+#     MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+#     IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+#     CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+#     TORT OR OTHERWISE, ARISING FROM,OUT OF OR IN CONNECTION WITH THE
+#     SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
+Help="
+Usage: $ScriptName $CommandName <pip-parameters>
+
+install, manage, and remove Python packages using 'pip'.
+
+Options:
+    -h, --help     show command help
+    -V, --version  show '$ScriptName' version and exit
+
+Examples:
+    $ScriptName $CommandName install <package>
+    $ScriptName $CommandName uninstall <package>
+    $ScriptName $CommandName show <package>
+    $ScriptName $CommandName freeze > requirements.txt
+"
+
+function run_command() {
+
+    require_virtual_python
+    virtual_python !pip "${CommandParams[@]}"
+
+}
