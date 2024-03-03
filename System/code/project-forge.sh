c6df7fa2dab910292f49022070a607da90f8fb4e
Martin Rizzo <martinrizzo@gmail.com>
2024-03-03 00:44:36 -0300

    feat: Add support for 'stable-diffusion-webui-forge' project
    This commit introduces the capability to manage the open-source
    'stable-diffusion-webui-forge' project within the aiman script.
    This project, which is hosted on GitHub, provides a web-based user
    interface for the popular Stable Diffusion text-to-image AI model.

---
+++ System/code/project-forge.sh
@@ -0,0 +1,98 @@
+#!/usr/bin/env bash
+# File    : project-forge.sh
+# Brief   : Controls the local copy of the "stable diffusion webui forge" project.
+# Author  : Martin Rizzo | <martinrizzo@gmail.com>
+# Date    : Mar 2, 2024
+# Repo    : https://github.com/martin-rizzo/AIMan
+# License : MIT
+#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+#                                    AIMan
+#        A basic package management system for AI open source projects
+#   
+#     Copyright (c) 2023-2024 Martin Rizzo
+#     
+#     Permission is hereby granted, free of charge, to any person obtaining
+#     a copy of this software and associated documentation files (the
+#     "Software"), to deal in the Software without restriction, including
+#     without limitation the rights to use, copy, modify, merge, publish,
+#     distribute, sublicense, and/or sell copies of the Software, and to
+#     permit persons to whom the Software is furnished to do so, subject to
+#     the following conditions:
+#     
+#     The above copyright notice and this permission notice shall be
+#     included in all copies or substantial portions of the Software.
+#     
+#     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+#     EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+#     MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+#     IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+#     CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+#     TORT OR OTHERWISE, ARISING FROM,OUT OF OR IN CONNECTION WITH THE
+#     SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
+
+
+function install() {
+    local hash_or_tag=$1
+    local project_dir=$(print_project @local_dir)
+    
+    require_system_command git wget
+    require_virtual_python
+    
+    change_to_repo_directory
+    clone_project_to "$project_dir" $hash_or_tag
+    cd "$project_dir"
+    require_soft_link 'output' '../../Output' 1
+    virtual_python launch.py --no-download-sd-model --exit    
+}
+
+function launch() {
+    local project_dir=$(print_project @local_dir)
+    local options=("$@")
+    local directories=()
+    
+    #-- ENABLE OPTIMIZATIONS --#
+    options+=(--pin-shared-memory) # for potential speed improvements.
+    options+=(--cuda-malloc)       # for potential speed improvements.
+    options+=(--cuda-stream)       # for potential speed improvements.
+    
+    #options+=(--opt-sdp-attention)         # non-deterministic, can be faster but uses more VRAM than xFormers
+    #options+=(--opt-sdp-no-mem-attention) # deterministic, can be faster but uses more VRAM than xFormers
+    #options+=(--xformers)                 # possibly no longer necessary with Torch 2
+    
+    #-- CONFIGURE USER SETTINGS --#
+    options+=(--listen)      # disable browser launch and allows connection from LAN
+    options+=(--theme dark)  # start in dark mode
+    #options+=(--autolaunch) # force browser launch even when --listen is enabled
+    
+    #-- REDIRECT DIRECTORIES FOR AIMAN --#
+    directories+=(--codeformer-models-path "$ModelsCodeformerDir")
+    directories+=(--embeddings-dir "$ModelsEmbeddingsDir")
+    directories+=(--esrgan-models-path "$ModelsEsrganDir")
+    directories+=(--gfpgan-models-path "$ModelsGfpganDir")
+    directories+=(--hypernetwork-dir "$ModelsHypernetworkDir")
+    directories+=(--ldsr-models-path "$ModelsLdsrDir")
+    directories+=(--lora-dir "$ModelsLoraDir")
+    directories+=(--realesrgan-models-path "$ModelsRealesrganDir")
+    directories+=(--scunet-models-path "$ModelsScunetDir")
+    directories+=(--ckpt-dir "$ModelsStableDiffusionDir")
+    directories+=(--swinir-models-path "$ModelsSwinirDir")
+    directories+=(--vae-dir "$ModelsVaeDir")
+    
+    change_to_repo_directory
+    cd "$project_dir"
+    require_soft_link 'output' '../../Output' 1
+    virtual_python launch.py "${options[@]}" "${directories[@]}"
+}
+
+function update() {
+    git pull
+}
+
+function revert() {
+    local hash=$(print_project @hash)
+    if [[ -n "$hash" ]]; then
+        git reset --hard "$hash"
+    fi
+}
+
